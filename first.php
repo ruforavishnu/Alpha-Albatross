d94d8f808c94f7a59bb1f94645c91acb0e23645c
Vishnu <vishnuajith310@gmail.com>
2021-06-07 10:21:10 +0530

    first commit

---
+++ first.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<?php
+
+	$title = "Welcome to my blog";
+	$content = "Thank you for visiting my blog. Let me begin by telling you about blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah"
+?>
+
+<h3>
+	<?php
+	echo $title;
+	?>
+
+</h3>
+<p>
+	<?php
+	echo $content;
+
+	?>
+</p>
+
+	
+</body>
+</html>
+
